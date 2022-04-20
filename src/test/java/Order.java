4b6e55e6b8fe289bd1039adda8e188f1be7a7984
fahmisyaputra14 <fahmisyaputra14>
2022-04-20 12:53:05 +0700

    update order

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,9 +19,31 @@ public class Order {
     }
 
     @Test
-    public void Flow ()
-    {
+    public void Flow () throws InterruptedException {
         driver.get(URL);
+        driver.manage().window().maximize();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div/div[1]/div/div/div[1]/div/div/div[1]/div/div[2]")).click();
+        Thread.sleep(14000);
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[3]/div/div/div[1]/input")).clear();
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[3]/div/div/div[1]/input")).sendKeys("Padang");
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[3]/div[2]/div/div/div[3]/div/div")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[3]/div[1]/div/div[1]/div[1]/input")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[3]/div[1]/div/div[1]/div[1]/input")).click();
+
+//        String Dari_expected = "Jakarta";
+//        String Dari_actual = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[1]/div/div/div[1]/input")).getText();
+//        //WebElement dari = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[1]/div/div/div[1]/input"));
+//        if (Dari_actual == Dari_expected)
+//        {
+//            driver.findElement(By.xpath("//*[@id=\"__next\"]/div[5]/div[2]/div[1]/div[2]/div/div[3]/div/div[2]/div/div[1]/div[1]/div[3]/div/div/div[1]/input")).sendKeys("Pad");
+//
+//        }
+        Thread.sleep(4000);
+
     }
 
     @AfterMethod
