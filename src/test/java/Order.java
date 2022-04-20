ac5c57a89ef37a7e628ca161ca1af70ab63bd408
fahmisyaputra14 <fahmisyaputra14>
2022-04-20 11:25:05 +0700

    kelima

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -1,6 +1,30 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Order {
     public WebDriver driver;
     public String URL = "https://www.traveloka.com/id-id/";
+
+    @BeforeMethod
+    public void beforeMethod ()
+    {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+        //Setting the driver to chrome driver
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void Flow ()
+    {
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void afterMethod()
+    {
+        driver.close();//close browser
+    }
 }
